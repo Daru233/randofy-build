0c48a4babb40e5a724dd57512149f0b7a57328f6
Michael Malto <michaeldm233@gmail.com>
2021-04-10 18:43:46 +0100

    tests

--- source/server.js
+++ source/server.js
@@ -10,4 +10,3 @@ var app_1 = __importDefault(require("./app"));
 var NAMESPACE = 'App';
 var httpServer = http_1.default.createServer(app_1.default);
 httpServer.listen(config_1.default.server.port, function () { return logging_1.default.info(NAMESPACE, "Server running on " + config_1.default.server.hostname + ":" + config_1.default.server.port); });
-// testing workflow
