e7b4e0ade40649b99ad617d657c8e577ffad6505
Michael Malto <michaeldm233@gmail.com>
2021-04-10 22:54:05 +0100

    123123

--- source/config/config.js
+++ source/config/config.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-var SERVER_HOSTNAME = process.env.SERVER_HOSTNAME;
-var SERVER_PORT = process.env.SERVER_PORT;
+var SERVER_HOSTNAME = process.env.HOSTNAME || "0.0.0.0"
+var PORT = process.env.PORT;
 var SERVER = {
     hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT
+    port: PORT
 };
 var config = {
     server: SERVER
